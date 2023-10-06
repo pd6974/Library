ccf8dfdd59d50b3a894b1346d3bb7e27d46a65c6
pd6974 <pdolan17@gmail.com>
2023-10-06 14:54:01 -0500

    Finished form inputs, centered form in side bar, added a heading, added a couple books to the array.  Need to make a function that displays the books in the main section of the webpage

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const myLibrary = [
+    {
+    title: "The Hobbit",
+    author: "JRR Tolkein",
+    genre: "Fantasy",
+    pages: "310",
+    read: false
+    },
+    
+    {
+    title: "Tiny, Beautiful Things",
+    author: "Cheryl Strayed",
+    genre: "Memoir",
+    pages: "368",
+    read: true
+    }
+];
+
+function Book(title, author, genre) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.pages = pages;
+    this.read = read;
+
+}
+
+function addBookToLibrary() {
+
+}
