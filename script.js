f398b8fe7b0c5d7a4d96bb52bd02e46275657b65
pd6974 <pdolan17@gmail.com>
2023-10-09 19:25:11 -0500

    Made function to display books in the main section.  Main section needs more designing and organization, need to finish hidden form element button situation, don't forget to comment js for easier coding

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
+//library length and main section to display books
+const libLength = myLibrary.length;
+const main = document.querySelector('.main');
+const formDiv = document.querySelector('.form')
+
+//variale to access form button
+const formButton = document.querySelector('.formButton');
+formButton.addEventListener('click', () => {
+    displayForm();
+})
+
+
 const myLibrary = [
     {
     title: "The Hobbit",
@@ -7,7 +19,7 @@ const myLibrary = [
     pages: "310",
     read: false
     },
-    
+
     {
     title: "Tiny, Beautiful Things",
     author: "Cheryl Strayed",
@@ -17,12 +29,39 @@ const myLibrary = [
     }
 ];
 
+
+
+displayLibrary();
+
+
 function Book(title, author, genre) {
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.pages = pages;
     this.read = read;
+
+}
+
+function displayForm {
+
+}
+
+
+//function to display library
+function displayLibrary() {
+    for (let i = 0; i < libLength; i++) {
+        const gridDiv = document.createElement('div');
+        main.appendChild(gridDiv);
+        gridDiv.classList.add('gridDiv');
+        for (const property in myLibrary[i]) {
+            var p = document.createElement('p');
+            gridDiv.appendChild(p);
+            p.textContent += `${myLibrary[i][property]}`
+
+        }
+            
+    }
 
 }
 
